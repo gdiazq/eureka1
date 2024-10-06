72c1d114961af91c82c9eaee8dead2576918fff1
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-10-05 23:33:01 -0300

    listo el getmapping

--- src/main/java/com/iteureka/semana1/controller/CustomerController.java
+++ src/main/java/com/iteureka/semana1/controller/CustomerController.java
@@ -1,7 +1,25 @@
 package com.iteureka.semana1.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.iteureka.semana1.service.CustomerService;
+import com.iteureka.semana1.model.CustomerEntity;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/customer")
+@AllArgsConstructor
 public class CustomerController {
 
-    
+    private final CustomerService customerService;
 
+    @GetMapping
+    public Iterable<CustomerEntity> getAllCustomer() {
+        return customerService.findAll();
+    }
+    
+    
 }
